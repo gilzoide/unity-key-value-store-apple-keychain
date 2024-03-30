56d945d2e336157e0e250c4894b5ff4a25d6b7a5
gilzoide <gilzoide@gmail.com>
2024-03-30 16:13:08 -0300

    Add some tooltips in GenericPasswordKeychainAttributes

--- Runtime/GenericPasswordKeychainAttributes.cs
+++ Runtime/GenericPasswordKeychainAttributes.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using UnityEngine;
 
 namespace Gilzoide.KeyValueStore.AppleKeychain
 {
@@ -15,15 +16,20 @@ namespace Gilzoide.KeyValueStore.AppleKeychain
         [MarshalAs(UnmanagedType.LPUTF8Str)]
         public string AccessGroup = null;
 
+        [Tooltip("User-visible label for this keychain item")]
         [MarshalAs(UnmanagedType.LPUTF8Str)]
         public string Label = null;
 
+        [Tooltip("User-visible string describing this kind of keychain item")]
         [MarshalAs(UnmanagedType.LPUTF8Str)]
         public string Description = null;
 
+        [Tooltip("Whether the keychain item in question is synchronized to other devices through iCloud")]
         [MarshalAs(UnmanagedType.I1)]
         public bool Synchronizable = false;
 
+        [Tooltip("Set the value for this key to true when accessing a macOS keychain item that behaves like an iOS keychain item. "
+            + "Affects operations only in macOS, other platforms automatically behave as if the key is set to true.")]
         [MarshalAs(UnmanagedType.I1)]
         public bool UseDataProtectionKeychain = false;
     }
